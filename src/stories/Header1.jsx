4bbdfa5f7e18849f54a79ff9924878ef63b695d7
tognia <h.tognia@ziyouma-agency.com>
2022-02-04 18:45:19 +0100

    Switch from Yarn to npm 0422022

--- src/stories/Header1.jsx
+++ src/stories/Header1.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import logo from './assets/logo.png'
 
-//import { Button } from '../../stories/Button';
+import { Button } from './Button';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="wrapper">
       <div>
-        {/* <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <g fill="none" fillRule="evenodd">
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
@@ -24,33 +23,30 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
               fill="#91BAF8"
             />
           </g>
-        </svg> */}
-        <div className='text-primary'>
-          <img src = {logo} width="50px" height="50px" alt='TodoAppGo' />
-                <h4>ToDoAppGo</h4>
-        </div>
+        </svg>
+        <h1>Acme</h1>
       </div>
       <div>
-        {/* {user ? (
+        {user ? (
           <Button size="small" onClick={onLogout} label="Log out" />
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
             <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
-        )} */}
+        )}
       </div>
     </div>
   </header>
 );
 
-// Header.propTypes = {
-//   user: PropTypes.shape({}),
-//   onLogin: PropTypes.func.isRequired,
-//   onLogout: PropTypes.func.isRequired,
-//   onCreateAccount: PropTypes.func.isRequired,
-// };
+Header.propTypes = {
+  user: PropTypes.shape({}),
+  onLogin: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  onCreateAccount: PropTypes.func.isRequired,
+};
 
-// Header.defaultProps = {
-//   user: null,
-// };
+Header.defaultProps = {
+  user: null,
+};
