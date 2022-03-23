3eecfd8f87a48873a2770a0ccc1b2f0a23724481
tognia <h.tognia@ziyouma-agency.com>
2022-03-23 16:55:20 +0100

    Tests sectionAddTask 23032022

--- ROADMAP.md
+++ ROADMAP.md
@@ -5,7 +5,10 @@
 
 **[IN PROGRESS] - [2.1.0] - [2022-03-13]**
  Expected delevery date of the first version that include the basic features
-- [] Proceed tests for button submit 
+- [x] Proceed tests for button submit 
+- [x] Proceed tests for InputTextNewTask
+- [x] Proceed tests for SectionAddTask
+- [x] Proceed tests for TaskHomePage  
 - [] Proceed tests coverage 5%
 - [] Auth
 - [x] Loader for Data
--- src/components/task/SectionAddTask/input.test.js
+++ src/components/task/SectionAddTask/input.test.js
@@ -1,12 +1,19 @@
-// InputTextNewTask Test File
+// SectionAddTask Test File
 
-import InputTextNewTask from './'
+// import SectionAddTask from './'
 import { screen, render, cleanup } from '@testing-library/react'
 import {fireEvent} from '@testing-library/react'
 import { unmountComponentAtNode} from "react-dom";
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import userEvent from '@testing-library/user-event'
+import React from 'react';
+import InputTextNewTask from '../InputTextNewTask/index'
+import ButtonSubmit from '../../ButtonSubmit';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+
+
+
 
 // afterEach(cleanup);
 
@@ -30,27 +37,42 @@ afterEach(() => {
   });
 
 
-describe('InputTextNewTask', () => {
+describe('SectionAddTask', () => {
 
+    var fakeDate = { selectedDay: "2022-3-18" }
 
-    test('InputTextNewTask Should render without crash', async () => {
-        render(<InputTextNewTask />)
-    })
-
-    test('InputTextNewTask - Should perform action when text changes', async () => {
-
-        var tt = "jajaj"
+    var tt = "jajaj"
         // const handleChange = jest.fn();
         var handleChange = ()=> {
                 tt = "nuvibz"
         }       
-        
+
+    function handleDayChange(d) {
+
+        fakeDate = { selectedDay: d}
+        console.log("Peaceful Journey", fakeDate.selectedDay.toString());
+      }
+    
+    // function handleChange(event) {
+
+    // }
+
+      
+    test('SectionAddTask display date well', async () => {
+        // render(<SectionAddTask />)
+        handleDayChange("2022-4-31".toString())
+        expect(fakeDate.selectedDay.toString()).toBe("2022-4-31".toString())
+    })
+
+    test('Text should change on typing', async () => {
+               
         
         render(            
         <InputTextNewTask myvalue={tt} handleChange={handleChange} data-testid="my-input" />        
         )
     const inputTest = screen.getByRole('textbox');
         // coTest = await screen.findByTestId(`my-input`);
+        console.log("Peace457", inputTest.value)
         
         
     expect(inputTest.value).toBe('jajaj'); // to test input value
@@ -61,14 +83,11 @@ describe('InputTextNewTask', () => {
 
     //  const InputEl = screen.getByTestId('my-input')
     
-     console.log("Peace457", inputTest1.value)
-     userEvent.clear(screen.getByRole('textbox', 'email'))
-    //  expect(inputTest1.value).toBe('nuvibz'); // to test input value
-    //  expect(screen.getByRole('textbox')).toHaveValue('nuvibz')
-    // expect(handleChange).toBeCalledWith('su'); // tests if onChange handler is called with proper value
-        
-             
+     console.log("MTN", inputTest1.value)
+    
+    })
+
+    
             
   
         })
-})
